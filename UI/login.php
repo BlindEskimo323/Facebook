4afc86476fa146f645971796ed750522874c8771
root <root@JAG-HP-PAVILION-DV6.(none)>
2014-03-21 14:50:26 +0000

    test change

--- UI/login.php
+++ UI/login.php
@@ -1,16 +1,38 @@
 <?php
 if(isset($_POST["login_button"])){
 	session_start();
-	mysql_connect("localhost", "root", "password");
-	mysql_select_db("Facebook");
+
+	$host = "du7n801d1x.database.windows.net,1433";
+	$user = "user";
+	$pwd = "Abcd1234";
+	$db = "Facebook";
+	// Connect to database.
+	try {
+	    $sql = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+	    $sql->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch(Exception $e){
+	    die(var_dump($e));
+	}
+	
+//	$sql = new mysqli("localhost", "root", "password", "Facebook");
+//	if($sql->connect_errno) {
+//		printf("Connection Failed: %s\n", $sql->connect_error);
+//		exit;
+//	}
 	$username = (isset($_POST["contact-uname"]) ? $_POST["contact-uname"]:null);
 	$password = (isset($_POST["contact-pwd"]) ? hash('sha256', $_POST["contact-pwd"]):null);
 	if($username != null && $password != null){
-		$query = sprintf("SELECT Username, Password FROM Users WHERE Username='%s'",
-		mysql_real_escape_string($username));
-		$result = mysql_fetch_assoc(mysql_query($query));
-		
-		if(strcmp($password, $result['Password'])==0){
+		$stmt = $sql->prepare("SELECT Username, Password FROM Users WHERE Username=?");
+		if(!$stmt){
+			printf("Query Prep Failed: %s\n", $sql->error);
+			exit;
+		}
+		$stmt->bind_param('s', $username);
+		$stmt->execute();
+		$stmt->bind_result($uname, $pass);
+		$stmt->fetch();
+		if(strcmp($password, $pass)==0){
 			$_SESSION['current_user'] = $username;
 			header("Location: index.php");
 			die();
